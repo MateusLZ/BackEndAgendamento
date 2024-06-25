6645907829b6b38375e0b170bcf449124cd513e3
mateus <mateuslimazeredo@hotmail.com>
2024-06-25 15:00:38 -0300

    sera que vai? agora de verdade sem erro

--- src/main/java/br/com/api/produtos/Seguranca/CorsConfig.java
+++ src/main/java/br/com/api/produtos/Seguranca/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("<https://seu-frontend.vercel.app>")
+                .allowedOrigins("https://cabeloeart.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
