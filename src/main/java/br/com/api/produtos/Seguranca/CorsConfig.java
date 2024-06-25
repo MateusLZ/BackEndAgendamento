5003194973ef077f84085211a020f78de507ffd0
mateus <mateuslimazeredo@hotmail.com>
2024-06-25 15:12:48 -0300

    oi

--- src/main/java/br/com/api/produtos/Seguranca/CorsConfig.java
+++ src/main/java/br/com/api/produtos/Seguranca/CorsConfig.java
@@ -2,18 +2,22 @@ package br.com.api.produtos.Seguranca;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://cabeloeart.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean 
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://cabeloeart.vercel.app"); // Adicione o domínio do seu frontend
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); 
+        config.addExposedHeader("Content-Disposition");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
