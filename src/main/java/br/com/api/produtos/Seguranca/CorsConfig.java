0617487691e6e580ea57847efcd90e7601ddd599
mateus <mateuslimazeredo@hotmail.com>
2024-06-25 14:59:58 -0300

    sera que vai? agora de verdade

--- src/main/java/br/com/api/produtos/Seguranca/CorsConfig.java
+++ src/main/java/br/com/api/produtos/Seguranca/CorsConfig.java
@@ -2,22 +2,18 @@ package br.com.api.produtos.Seguranca;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean 
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://cabeloeart.vercel.app"); // Adicione o domínio do seu frontend
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*"); 
-        config.addExposedHeader("Content-Disposition");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("<https://seu-frontend.vercel.app>")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
--- src/main/java/br/com/api/produtos/controle/ApiController.java
+++ src/main/java/br/com/api/produtos/controle/ApiController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
 public class ApiController {
 
+@CrossOrigin(origins = "https://cabeloeart.vercel.app")
     @GetMapping("/dados")
     public ResponseEntity<String> getDados() {
         return ResponseEntity.ok("Dados da API");
